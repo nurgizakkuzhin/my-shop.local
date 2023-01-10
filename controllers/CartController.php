21bcb46148863142849116e68bbf84fc8c4d9636
nurgizakkuzhin <akkuzhin.n@gmail.com>
2023-01-10 18:30:00 +0300

    #5.2 - Страница заказа

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -95,4 +95,60 @@ function orderAction($smarty)
         redirect('/cart/');
         return;
     }
+
+    //получаем из массива $_POST количество покупаемых товаров
+    $itemsCnt = [];
+    foreach ($itemsIds as $item) {
+        //формируем ключ для массива POST
+        $postVar = 'itemsCnt_' . $item;
+        //создаем элемент массива количества покупаемых товаров
+        //ключ массива - ID товара, значение массива - количество товара
+        //$itemsCnt[1] = 3; товар с ID == 1 покупают 3 штуки
+        $itemsCnt[$item] = isset($_POST[$postVar]) ? $_POST[$postVar] : null;
+    }
+
+    //получаем список продуктов по массиву корзины
+    $rsProducts = getProductsFromArray($itemsIds);
+
+    //добавляем каждому продукту дополнительное поле
+    //"realPrice" = количество продуктов * на цену продукта
+    //"cnt" = количество покупаемого товара
+
+    //&item - для того чтобы при изменении переменной $item
+    //менялся и элемент массива $rsProducts
+    $i = 0;
+    foreach ($rsProducts as &$item) {
+        $item['cnt'] = isset($itemsCnt[$item['id']]) ? $itemsCnt[$item['id']] : 0;
+        if ($item['cnt']) {
+            $item['realPrice'] = $item['cnt'] * $item['price'];
+        } else {
+            //если вдруг получилось так что товар в корзине есть, а количество == нулю
+            //то удаляем этот товар
+            unset($rsProducts[$i]);
+        }
+        $i++;
+    }
+
+    if (! $rsProducts) {
+        echo "Корзина пуста";
+        return;
+    }
+
+    //полученный массив покупаемых товаров помещаем в сессионную переменную
+    $_SESSION['saleCart'] = $rsProducts;
+
+    $rsCategories = getAllMainCatsWithChildren();
+
+    //hideLoginBox переменная - флаг для того чтобы спрятать блоки логина и регистрации в боковой панели
+    if (! isset($_SESSION['user'])) {
+        $smarty->assign('hideLoginBox', 1);
+    }
+
+    $smarty->assign('pageTitle', 'Заказ');
+    $smarty->assign('rsCategories', $rsCategories);
+    $smarty->assign('rsProducts', $rsProducts);
+
+    loadTemplate($smarty, 'header');
+    loadTemplate($smarty, 'order');
+    loadTemplate($smarty, 'footer');
 }
